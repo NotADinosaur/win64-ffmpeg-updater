376df77299c02b3c6397eb684276db9b273d2dd0
NotADinosaur <yalnif9@gmail.com>
2022-08-12 17:16:51 -0700

    reformat w/ functions, add timer

--- ffupdate.py
+++ ffupdate.py
@@ -1,14 +1,24 @@
 #ffmpeg updater by zozo
 #initial ver. 2022-08-11
+#last updated 2022-08-12
 
-import requests, zipfile
+import requests, zipfile, time
 
 #download and save latest precompiled ffmpeg build from BtbN
-download = requests.get("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip")
-open("ffmpeg.zip", "wb").write(download.content)
+def download():
+    print("downloading...")
+    downloadFile = requests.get("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip")
+    open("ffmpeg.zip", "wb").write(downloadFile.content)
 
 #extract downloaded zip file
-with zipfile.ZipFile("ffmpeg.zip", "r") as ffmpeg:
-    ffmpeg.extractall()
+def extract():
+    print("extracting...")
+    with zipfile.ZipFile("ffmpeg.zip", "r") as ffmpeg:
+        ffmpeg.extractall()
 
-print("done!")
+startTime = time.time()
+download()
+extract()
+finishTime =  time.time()
+timeLapsed = time.strftime("%Mm %Ss", time.gmtime(finishTime - startTime))
+print("done! in " + str(timeLapsed))
