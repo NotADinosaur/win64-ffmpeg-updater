bc4ef5e67ef750709c733a0a8a4476bda5d43e2e
NotADinosaur <yalnif9@gmail.com>
2023-08-01 10:57:10 -0700

    I don't even remember what half this shit does (begin rewrite)

--- ffupdate.py
+++ ffupdate.py
@@ -1,81 +1,54 @@
 #ffmpeg updater by zozo
 #initial ver. 2022-08-11
-#last updated 2022-09-23
+#last updated 2023-07-31
 
 import requests, zipfile, time, colorama, os, sys
 
-#download and save latest precompiled ffmpeg build from BtbN
-def download():
-    print("downloading...")
-    try:
-        downloadFile = requests.get("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip", timeout = 60)
-    #throw a connection error if something goes wrong
-    except requests.exceptions.RequestException:
-        print("\033[0;31mconnection error: \033[0;0m" + "an error occured while downloading ffmpeg")
-        #delete any partially downloaded files
-        if os.path.isfile("ffmpeg.zip") == True:
-            os.remove("ffmpeg.zip")
-        input("press any key to exit")
-        sys.exit()
-    else:
-        with open("ffmpeg.zip", "wb") as save:
-            save.write(downloadFile.content)
-
-
-#extract downloaded zip file
-def extract():
-    print("extracting...")
-    with zipfile.ZipFile("ffmpeg.zip", "r") as ffmpeg:
-        for file in ffmpeg.infolist():
-            if file.is_dir() == False:
-                if getDocs == True:
-                    if "bin" in file.filename:
-                        file.filename = os.path.basename(file.filename)
-                        ffmpeg.extract(file, extBin)
-                    elif "doc" in file.filename:
-                        file.filename = os.path.basename(file.filename)
-                        ffmpeg.extract(file, extDoc)
-                    else:
-                        file.filename = os.path.basename(file.filename)
-                        ffmpeg.extract(file, extDir)
-                elif getDocs == False:
-                    if "bin" in file.filename:
-                        file.filename = os.path.basename(file.filename)
-                        ffmpeg.extract(file, extBin)
-    #delete zip after extracting contents
-    os.remove("ffmpeg.zip")
-
-#if config file exists, allow a few small changes
-def config():
-    global extBin
-    global extDoc
-    global extDir
-    global getDocs
-    if os.path.isfile("config.txt") == True:
-        with open("config.txt", "r") as config:
-            for line in config:
-                if line.startswith("extDir") == True:
-                    extDir = line.replace("extDir = ", "").strip()
-                    extBin = extDir + "\\bin"
-                    extDoc = extDir + "\\doc"
-                elif line.startswith("getDocs") == True:
-                    getDocs = line.replace("getDocs = ", "").strip()
-                    if getDocs == "True":
-                        getDocs = True
-                    elif getDocs == "False":
-                        getDocs = False
-    else:
-        extDir = os.getcwd()
-        extBin = extDir + "\\bin"
-        extDoc = extDir + "\\doc"
-        getDocs = True
+#temporary variables between config file and arguments
+extDir = os.getcwd()
+extBin = extDir + "\\bin"
+extDoc = extDir + "\\doc"
+getDocs = True
 
 #program starts here
 colorama.init()
 startTime = time.time()
-config()
-download()
-extract()
+#download and save latest precompiled ffmpeg build from BtbN
+print("downloading...")
+try:
+    downloadFile = requests.get("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip", timeout = 60)
+#throw a connection error if something goes wrong
+except requests.exceptions.RequestException:
+    print("\033[0;31mconnection error: \033[0;0m" + "an error occured while downloading ffmpeg")
+    #delete any partially downloaded files
+    if os.path.isfile("ffmpeg.zip") == True:
+        os.remove("ffmpeg.zip")
+    input("press any key to exit")
+    sys.exit()
+else:
+    with open("ffmpeg.zip", "wb") as save:
+        save.write(downloadFile.content)
+#extract downloaded zip file
+print("extracting...")
+with zipfile.ZipFile("ffmpeg.zip", "r") as ffmpeg:
+    for file in ffmpeg.infolist():
+        if file.is_dir() == False:
+            if getDocs == True:
+                if "bin" in file.filename:
+                    file.filename = os.path.basename(file.filename)
+                    ffmpeg.extract(file, extBin)
+                elif "doc" in file.filename:
+                    file.filename = os.path.basename(file.filename)
+                    ffmpeg.extract(file, extDoc)
+                else:
+                    file.filename = os.path.basename(file.filename)
+                    ffmpeg.extract(file, extDir)
+            elif getDocs == False:
+                if "bin" in file.filename:
+                    file.filename = os.path.basename(file.filename)
+                    ffmpeg.extract(file, extBin)
+#delete zip after extracting contents
+os.remove("ffmpeg.zip")
 finishTime =  time.time()
 #convert time into human-readable format
 if float(time.strftime("%M", time.gmtime(finishTime - startTime))) < 1:
